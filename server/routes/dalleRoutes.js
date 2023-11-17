6bdfb8a5036d47cb381783fb34b647af809e59e9
josephmolina256 <128499367+josephmolina256@users.noreply.github.com>
2023-11-17 17:23:02 -0500

    commit 1

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -8,12 +8,32 @@ dotenv.config()
 const router = express.Router()
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 
 router.route('/').get((req, res)=>{
     res.send("hello from dalle")
+})
+
+router.route('/').post(async (req, res) =>{
+    try {
+        const {prompt} = req.body
+
+        const aiResponse = await openai.images.generate({
+            model: "dall-e-2",
+            prompt,
+            n:1,
+            size: '256x256',
+            response_format: 'b64_json',
+        })
+        const image = aiResponse.data[0].b64_json
+
+        res.status(200).json({photo: image})        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error?.response.data.error.message)
+    }
 })
 
 export default router
