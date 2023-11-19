af36490b418ac17ac3d3b3ac48742f90e3a06d9b
josephmolina256 <128499367+josephmolina256@users.noreply.github.com>
2023-11-18 20:44:51 -0500

    Header updates

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -81,7 +81,7 @@ const CreatePost = () => {
         <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
         <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
         Create imaginative and visually 
-        stunning images through DALL-E AI and share them with the community </p>
+        stunning images through  uiGen and share them with the community </p>
         <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>You can see some prompt ideas by clicking the Suprise Me button below</p>
       </div>
       <form className='mt-16 max-w-2xl' onSubmit={handleSubmit}>
